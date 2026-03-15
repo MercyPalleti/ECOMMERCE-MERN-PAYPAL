c368213e54bfae1c3ba0e08745223fa8b8e234da
MercyPalleti <palletimercy1@gmail.com>
2026-03-15 23:12:01 +0530

    API_url

--- client/src/store/admin/order-slice/index.js
+++ client/src/store/admin/order-slice/index.js
@@ -1,6 +1,9 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
+
 const initialState = {
   orderList: [],
   orderDetails: null,
@@ -10,7 +13,7 @@ export const getAllOrdersForAdmin = createAsyncThunk(
   "/order/getAllOrdersForAdmin",
   async () => {
     const response = await axios.get(
-      `http://localhost:5000/api/admin/orders/get`
+      `${API_BASE_URL}/admin/orders/get`
     );
 
     return response.data;
@@ -21,7 +24,7 @@ export const getOrderDetailsForAdmin = createAsyncThunk(
   "/order/getOrderDetailsForAdmin",
   async (id) => {
     const response = await axios.get(
-      `http://localhost:5000/api/admin/orders/details/${id}`
+      `${API_BASE_URL}/admin/orders/details/${id}`
     );
 
     return response.data;
@@ -32,7 +35,7 @@ export const updateOrderStatus = createAsyncThunk(
   "/order/updateOrderStatus",
   async ({ id, orderStatus }) => {
     const response = await axios.put(
-      `http://localhost:5000/api/admin/orders/update/${id}`,
+      `${API_BASE_URL}/admin/orders/update/${id}`,
       {
         orderStatus,
       }
